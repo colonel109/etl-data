51b6466d60499c32754b72e9d687c797d2def2cc
duong_desktop <duong80559@gmail.com>
2026-07-09 17:20:49 +0700

    tạo kết nối cho file khởi tạo db

--- init_database.py
+++ init_database.py
@@ -1,5 +1,6 @@
 from src.database.client import DatabaseClient
 from src.database.structure import Base
-
-client = DatabaseClient()
+from sqlalchemy import create_engine
+engine = create_engine("postgresql+psycopg://postgres:duong1234@localhost:5432/daesang_db_test")
+client = DatabaseClient(engine)
 client.create_table()
