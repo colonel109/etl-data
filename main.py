2aac44b2479fcba513d5a455b86c72965d0c1ff0
duong_desktop <duong80559@gmail.com>
2026-07-15 08:56:57 +0700

    thêm logic chọn file cho pipeline xử lí dữ liệu bán hàng

--- main.py
+++ main.py
@@ -4,20 +4,27 @@ from src.data_processor import SalesDataProcessor
 from src.database.controller import DatabaseController
 from src.data_inspector import DebugViewInspector
 from src.helper.result_writer import ResultWriter
+from src.pipeline_selector import PipelineSelector
 
 
 class MainPipeline:
-    def __init__(self, base_path, engine):
+    def __init__(self, base_path, data_path, engine):
         self.base_path = base_path
         self.engine = engine
         self.database_controller = DatabaseController(self.engine)
         self.view_debugger = DebugViewInspector(self.engine)
         self.sales_data_processor= SalesDataProcessor(self.engine)
         self.result_writer = ResultWriter(base_path)
+        self.pipeline = PipelineSelector(data_path)
+    
+    def select_file_path(self):
+        folder_list = self.pipeline.select_folder()
+        file_paths = self.pipeline.select_file(folder_paths=folder_list)
+        return file_paths
         
-    def sales_data_pipeline(self, file_paths):
+    def process_file(self, file_paths):
         """
-        Pipeline xử lí dữ liệu bán hàng
+        Xử lí dữ liệu và import vào database
         """
         
         result, has_error = self.sales_data_processor.read_excel(file_path_list=file_paths)
@@ -40,10 +47,20 @@ class MainPipeline:
             return 
         
         self.sales_data_processor.main_table_importer()
-        
-main_pipeline = MainPipeline(
-    base_path=Path().cwd(),
-    engine=create_engine("postgresql+psycopg://postgres:duong1234@localhost:5432/daesang_db_test")
-)
 
-main_pipeline.sales_data_pipeline(file_paths = [file for file in Path("data/sales/daily").glob("*")])
+BASE_PATH = Path().cwd()
+DATA_PATH = BASE_PATH / "data"
+
+if __name__ == "__main__":    
+    main_pipeline = MainPipeline(
+        base_path=BASE_PATH,
+        data_path=DATA_PATH,
+        engine=create_engine("postgresql+psycopg://postgres:duong1234@localhost:5432/daesang_db_test")
+    )
+
+    file_paths = main_pipeline.select_file_path()
+
+    if file_paths:
+        main_pipeline.process_file(
+            file_paths=file_paths
+        )
---
+++ src/pipeline_selector.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+import questionary
+
+
+class PipelineSelector:
+    """
+    Chọn pipeline, file cần xử lí. Trả về danh sách các file, bảng cần import
+    """
+
+    def __init__(self, data_path: Path):
+        self.data_path = data_path
+
+    def select_folder(self):
+        """
+        Lấy các folder dựa vào pipeline được chọn
+        """
+        
+        pipeline = {
+            "Dữ liệu bán hàng": {
+                "sales": {
+                    "Dữ liệu tháng": {"AR Invoice": "ar_invoice", "Order": "order"},
+                    "Dữ liệu ngày": "daily"
+                }
+            },
+            "Dữ liệu lỗ lãi": "profit_and_loss",
+        }
+
+        # Lựa chọn pipeline
+        pipeline_select = questionary.select(
+            "Chọn pipeline cần xử lí:",
+            choices=list(pipeline.keys())
+        ).ask()
+
+        selected_pipeline = pipeline[pipeline_select]
+
+        # Chọn pipeline xử lí dữ liệu lỗ lãi
+        if pipeline_select == "Dữ liệu lỗ lãi":
+            path = self.data_path / selected_pipeline
+            return path
+
+        # Chọn piple xử lí dữ liệu bán hàng
+        if pipeline_select == "Dữ liệu bán hàng":
+            report_type_dict = selected_pipeline["sales"]
+            sales_type = questionary.select(
+                "Chọn loại bảng cần import:",
+                choices=list(report_type_dict.keys())
+            ).ask()
+
+            file_type_dict = report_type_dict[sales_type]
+            if sales_type == "Dữ liệu ngày":
+                return self.data_path / "sales" / file_type_dict
+            
+            if sales_type == "Dữ liệu tháng":
+                file_type = questionary.checkbox(
+                    "Chọn loại file cần xử lí",
+                    choices = list(file_type_dict.keys())
+                ).ask()
+
+                path = [
+                    self.data_path / "sales" / file_type_dict[item]
+                    for item in file_type
+                ]
+                return path
+    
+    def select_file(self, folder_paths: list):
+        """
+        Lấy các path của các file được chọn từ danh sách folder được truyển vào
+        """
+        
+        selected_files = []
+        folders = folder_paths
+
+        if folders:
+            for folder in folders:
+                if not folder.exists():
+                    continue
+
+                files = [f for f in folder.iterdir() if f.is_file()]
+
+                if not files:
+                    continue
+
+                file_map = {f.name: f for f in files}
+
+                chosen_names = questionary.checkbox(
+                    f"Chọn các file trong thư mục [{folder.name}]:",
+                    choices=list(file_map.keys()),
+                ).ask()
+
+                if chosen_names:
+                    for name in chosen_names:
+                        selected_files.append(file_map[name])
+        
+        return selected_files
