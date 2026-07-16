d34c8ff2e3bcf8ad53d1e782ae19126a20c98271
duong_desktop <duong80559@gmail.com>
2026-07-16 11:58:13 +0700

    bổ sung cấu trúc của bảng profit and loss

--- init_database.py
+++ init_database.py
@@ -1,5 +1,5 @@
-from src.database.structure import Base, TransactionsDaily
+from src.database.structure import Base, ProfitAndLossStaging, ProfitAndLoss
 from sqlalchemy import create_engine
 engine = create_engine("postgresql+psycopg://postgres:duong1234@localhost:5432/daesang_db_test")
 
-Base.metadata.create_all(bind=engine, tables=[TransactionsDaily.__table__])
+Base.metadata.create_all(bind=engine, tables=[ProfitAndLossStaging.__table__, ProfitAndLoss.__table__])
