63866057d7238957c094a2633df3df7830911fb1
duong_desktop <duong80559@gmail.com>
2026-07-29 10:58:29 +0700

    thêm hàm điều phối cho class refresh report

--- src/report_refresher.py
+++ src/report_refresher.py
@@ -11,23 +11,27 @@ class ReportRefresher:
     """
     
     def __init__(self, report_folder: Path):
-        self.selected_report = None # Lưu trữ các path của các report được chọn để làm mới dữ liệu
         self.template_path = Path(report_folder / "template_report") # Thư mục chứa file mẫu
         self.result_path = Path(report_folder / "refreshed_report") # Thư mục chứa file kết quả sau khi refresh file mẫu
-       
-        self.file_selector() 
     
+    def run_process(self):
+        file = self.file_selector()
+        if file:
+            self.process_file(
+                selected_paths = file
+            ) 
+       
     def file_selector(self):
         """
         Chọn các file template, trả về một list các đường dẫn 
         """
         
-        self.selected_report = questionary.checkbox(
+        selected_report = questionary.checkbox(
             "Chọn báo cáo cần refresh dữ liệu",
             choices = [str(file) for file in self.template_path.glob("*.xlsx")]
         ).ask()
 
-        if not self.selected_report:
+        if not selected_report:
             return
         
         # Lấy danh sách các file đã tạo trước đó trong thư mục result
@@ -39,8 +43,10 @@ class ReportRefresher:
                 for rf in refreshed_file_name:
                     if f in rf.stem:
                         rf.unlink()
+        
+        return selected_report
 
-    def process_file(self):
+    def process_file(self, selected_paths: list):
         """
         Làm mới dữ liệu, đổi tên file theo ngày, giờ và di chuyển đến thư mục kết quả
         """
@@ -53,7 +59,7 @@ class ReportRefresher:
         excel.EnableEvents = False
         excel.AskToUpdateLinks = False
 
-        for path in self.selected_report:
+        for path in selected_paths:
             file_path = Path(path)
             if not file_path.is_file():
                 print(f"Không tìm thấy đường dẫn {path}")
